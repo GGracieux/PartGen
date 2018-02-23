bb9a519c870849d1d6f245c8be8c7239524a2f76
Guillaume Gracieux <ggracieux@gmail.com>
2018-02-23 17:09:00 +0100

    Ajout d'un footer

---
+++ source/front/src/cnb-editor/cnb-editor-footer/cnb-editor-footer.component.ts
@@ -0,0 +1,16 @@
+// Imports core
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'cnb-editor-footer',
+    styleUrls: ['./cnb-editor-footer.component.css'],
+    templateUrl: './cnb-editor-footer.component.html'
+})
+
+export class CnbEditorFooterComponent implements OnInit {
+
+    constructor() {}
+
+    public ngOnInit() {}
+	
+}
