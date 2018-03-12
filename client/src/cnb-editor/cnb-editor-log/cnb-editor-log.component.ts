4f5a1ac92da97576ef1d426f73b33a6ba8558905
Guillaume Gracieux <ggracieux@gmail.com>
2018-03-13 00:11:45 +0100

    First attempt to use modal

--- client/src/cnb-editor/cnb-editor-log/cnb-editor-log.component.ts
+++ client/src/cnb-editor/cnb-editor-log/cnb-editor-log.component.ts
@@ -1,5 +1,5 @@
 // Imports core
-import {Component, Input} from '@angular/core';
+import {Component, ViewChild, Input} from '@angular/core';
 
 // Imports du composant
 import {LogEntry} from './cnb-editor-log.interface';
@@ -12,7 +12,30 @@ import {LogEntry} from './cnb-editor-log.interface';
 
 export class CnbEditorLogComponent {
 
-    // Contenu des logs
+    // Log content
     @Input() content: LogEntry[];
+
+
+    // Visible switch
+    private _show;
+    get show(): boolean {
+        return this._show;
+    }
+    @Input() set show(show: boolean) {
+        this._show = show;
+        this.setVisibility(show);
+    }
+
+    // Show / Hide Modal
+    @ViewChild('demoBasic') public contentModal;
+    private setVisibility(visible: boolean) {
+        if (visible) {
+        //
+            //    this.contentModal.show();
+        } else {
+            //this.contentModal.hide();
+        }
+    }
+
 
 }
