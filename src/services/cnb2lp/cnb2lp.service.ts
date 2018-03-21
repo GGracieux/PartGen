53f926c021547a6a83d5d35acfe71a5c86396ffe
Guillaume Gracieux <ggracieux@gmail.com>
2018-03-21 15:41:27 +0100

    removing console log

--- src/services/cnb2lp/cnb2lp.service.ts
+++ src/services/cnb2lp/cnb2lp.service.ts
@@ -131,7 +131,6 @@ export class Cnb2lpService {
         {
             result = this.getConvertResponse(false,'Error while converting', e.message);
         }
-        console.log(result);
         return result;
 
     }
