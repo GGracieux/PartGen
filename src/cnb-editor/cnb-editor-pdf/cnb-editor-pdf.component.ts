b66ba5365428d95732e7fc42b67765c0ecd22dc0
Guillaume Gracieux <ggracieux@gmail.com>
2018-03-21 09:50:58 +0100

    Simplifying pdf setter

--- src/cnb-editor/cnb-editor-pdf/cnb-editor-pdf.component.ts
+++ src/cnb-editor/cnb-editor-pdf/cnb-editor-pdf.component.ts
@@ -18,27 +18,18 @@ export class CnbEditorPdfComponent {
 	// PDF binary content
 	public binaryContent;
 
-	// Base64 PDF content
-	private _b64Pdf;
-
 	// Base64 PDF content : Setter
 	@Input() 
 	set b64Pdf(b64Pdf: string) {
-    	this._b64Pdf = b64Pdf;
-    	this.loadPdf(this._b64Pdf);
+        this.binaryContent = this.base64ToArrayBuffer(b64Pdf);
 	}
-
-	// Load PDF from base64 content
-    public loadPdf(base64pdf) {
-    	this.binaryContent = this.base64ToArrayBuffer(base64pdf);
-    }
 
     // Base64 decode
 	private base64ToArrayBuffer(base64) {
-	    var binary_string =  window.atob(base64);
-	    var len = binary_string.length;
-	    var bytes = new Uint8Array( len );
-	    for (var i = 0; i < len; i++)        {
+	    let binary_string =  window.atob(base64);
+        let len = binary_string.length;
+        let bytes = new Uint8Array( len );
+	    for (let i = 0; i < len; i++)        {
 	        bytes[i] = binary_string.charCodeAt(i);
 	    }
 	    return bytes.buffer;
