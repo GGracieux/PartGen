0798fb682868f2ce243e81f3dc543997bba434ea
Guillaume Gracieux <ggracieux@gmail.com>
2018-03-25 22:30:32 +0200

    Moved kubernetes files to bps-orchestration repository

--- kubernetes/gcloud-conf.sh
+++
@@ -1,7 +0,0 @@
-# -----------------------------
-# CONFIGURING GCLOUD
-# -----------------------------
-
-gcloud config set project bagpipescores
-gcloud config set compute/zone europe-west1-b
-gcloud container clusters get-credentials cluster-bps
